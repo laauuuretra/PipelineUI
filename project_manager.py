402f5a9bdc842dce4d0050156f8681835864009c
Laurel <lauyapu@gmial.com>
2024-10-18 11:50:50 +0200

    Fix search files and get path

--- project_manager.py
+++ project_manager.py
@@ -5,7 +5,7 @@ class ProjectManager(object):
         self.main_path=production_path
 
     def search_files(self):
-        project_folder=[]
+        project_folder=[""]
 
         if os.path.exists(self.main_path) and os.path.isdir(self.main_path):
             for i in os.listdir(self.main_path):
@@ -16,3 +16,10 @@ class ProjectManager(object):
 
         return project_folder
 
+    def update_project_path(self, project_name_menu):
+        selected_project=project_name_menu.currentText()
+        project_path=""
+        if selected_project:
+            project_path=os.path.join(self.main_path,selected_project)
+
+        return project_path
